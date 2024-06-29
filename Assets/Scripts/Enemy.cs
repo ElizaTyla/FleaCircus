64614f615ab46a1aed3fd2e8e2fa2a83bd13cad8
sean <seanwadsworth16@gmail.com>
2024-06-29 12:21:25 +0100

    Added patrolling enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Transform currentPoint;
+    public float speed;
+    private GameObject flea;
+    public float hp = 10;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
+        flea = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMovement();
+
+        if (hp <= 0)
+        {
+            //enemy dies, drops blood
+        }
+    }
+
+    void EnemyMovement()
+    {
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f)
+        {
+            currentPoint = (currentPoint == pointA.transform) ? pointB.transform : pointA.transform;
+        }
+    }
+}
--- Assets/Scripts/Flea/Flea.cs
+++ Assets/Scripts/Flea/Flea.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +8,35 @@ public class Flea : MonoBehaviour
     public int hp;
     private bool invulnerable = false;
     public float invulnerableTime;
+    private Rigidbody2D rb;
 
-    public IEnumerable SetInvulnerable()
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            //dies, offer to restart?
+        }
+    }
+
+    IEnumerable SetInvulnerable()
     {
         invulnerable = true;
         yield return new WaitForSeconds(invulnerableTime);
         invulnerable = false;
     }
 
-    public void ChangeHp(int amount)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (!invulnerable)
+        if (other.gameObject.CompareTag("Enemy") && !invulnerable)
         {
-            hp += amount;
-
+            hp -= 5;
+            StartCoroutine("SetInvulnerable");
         }
+        rb.AddForce(new Vector2(-2, 2));
     }
 }
